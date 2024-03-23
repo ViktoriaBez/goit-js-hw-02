663691e69f6fb1b070a832884fbb9153871457ee
ViktoriaBez <154977573+ViktoriaBez@users.noreply.github.com>
2024-03-23 19:38:46 +0200

    step 3 4

--- js/task-1.js
+++ js/task-1.js
@@ -7,7 +7,6 @@ function makeTransaction(quantity, pricePerDroid, customerCredits) {
     }
 }
 
-
 console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
 console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
